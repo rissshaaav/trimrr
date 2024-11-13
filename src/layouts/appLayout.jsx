5ecfafb9ac0a34175edc0cc3d918d237626fc876
Rishav <111266613+rissshaaav@users.noreply.github.com>
2024-11-13 22:04:50 +0530

    -setup nav bar -get shadcn button, dropdown, avatar, accordion and input component

--- src/layouts/appLayout.jsx
+++ src/layouts/appLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Header from '@/components/Header'
 import { Outlet } from 'react-router-dom'
 
 const AppLayout = () => {
   return (
     <div>
       <main className='min-h-screen container'>
-        {/* header */}
+        <Header/>
         <Outlet/>
       </main>
       <div className='p-10 text-center bg-gray-800 mt-10'>
