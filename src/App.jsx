a4c9577c6d3de6714aec4b8ba6cba1f153058864
Rishav <111266613+rissshaaav@users.noreply.github.com>
2024-11-14 11:58:11 +0530

    -wrap routerprovider in urlprovider

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,43 @@ import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 import Link from "./pages/Link";
 import RedirectLink from "./pages/RedirectLink";
+import UrlProvider from "./context";
+
 
 const router = createBrowserRouter([
-  {
-    element: <AppLayout/>,
-    children: [
-        {
-            path: '/',
-            element: <Landing/>
-        },
-        {
-            path: '/auth',
-            element: <Auth/>
-        },
-        {
-            path: '/dashboard',
-            element: <Dashboard/>
-        },
-        {
-            path: '/link/:id',
-            element: <Link/>
-        },
-        {
-            path: '/:id',
-            element: <RedirectLink/>
-        },
-    ] 
-  }
-])
+    {
+        element: <AppLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Landing />,
+            },
+            {
+                path: "/auth",
+                element: <Auth />,
+            },
+            {
+                path: "/dashboard",
+                element: <Dashboard />,
+            },
+            {
+                path: "/link/:id",
+                element: <Link />,
+            },
+            {
+                path: "/:id",
+                element: <RedirectLink />,
+            },
+        ],
+    },
+]);
 
 function App() {
-    return <RouterProvider router={router} />;
+    return (
+        <UrlProvider>
+            <RouterProvider router={router} />;
+        </UrlProvider>
+    );
 }
 
 export default App;
