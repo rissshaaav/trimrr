8c0157ced116459e30a9b1693d9d88d4a321ad34
Rishav <111266613+rissshaaav@users.noreply.github.com>
2024-11-14 12:33:52 +0530

    -create RequireAuth component to redirect unauthenticated users

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Dashboard from "./pages/Dashboard";
 import Link from "./pages/Link";
 import RedirectLink from "./pages/RedirectLink";
 import UrlProvider from "./context";
-
+import RequireAuth from "./components/RequireAuth";
 
 const router = createBrowserRouter([
     {
@@ -23,11 +23,19 @@ const router = createBrowserRouter([
             },
             {
                 path: "/dashboard",
-                element: <Dashboard />,
+                element: (
+                    <RequireAuth>
+                        <Dashboard />
+                    </RequireAuth>
+                ),
             },
             {
                 path: "/link/:id",
-                element: <Link />,
+                element: (
+                    <RequireAuth>
+                        <Link />
+                    </RequireAuth>
+                ),
             },
             {
                 path: "/:id",
