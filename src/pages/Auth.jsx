c72ca9f53db9ed43cdb5ef61562623fa7bc89d62
Rishav <111266613+rissshaaav@users.noreply.github.com>
2024-11-14 12:05:41 +0530

    -create signup ui -modify auth to navigate to dashboard if already logged in

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,10 +1,20 @@
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Login from "@/components/Login";
 import Signup from "@/components/Signup";
+import { UrlState } from "@/context";
+import { useEffect } from "react";
 
 const Auth = () => {
     let [searchParams] = useSearchParams();
+    const navigate = useNavigate();
+    const { isAuthenticated, loading } = UrlState();
+    const longLink = searchParams.get("createNew");
+
+    useEffect(() => {
+        if (isAuthenticated && !loading)
+            navigate(`/dashboard?${longLink ? `createNew=${longLink}` : ""}`);
+    }, [isAuthenticated, loading, navigate]);
     return (
         <div className="mt-36 flex flex-col items-center gap-10">
             <h1 className="text-5xl font-extrabold">
